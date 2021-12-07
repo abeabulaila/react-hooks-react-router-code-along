8ac6e9ddbefc03d5041670c00ee01d679f40aadc
Ibrahim Abulaila <abeabulaila@gmail.com>
2021-12-07 17:55:42 -0500

    ok

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Login from "./Login";
+import About from "./About";
+import Home from "./Home";
+import NavBar from "./NavBar";
+// Step 1. Import react-router functions
+import { BrowserRouter, Route, Switch, NavLink } from "react-router-dom";
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
-
-ReactDOM.render(<Home />, document.getElementById("root"));
+// Step 2. Change so router is coordinating what is displayed
+ReactDOM.render(
+  <BrowserRouter>
+    <NavBar />
+    <Switch>
+      <Route path="/login"><Login /></Route>
+      <Route path="/about"><About /></Route>
+      <Route exact path="/"><Home /></Route>
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
